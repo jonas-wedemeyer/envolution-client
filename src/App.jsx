153e8e73b77370482c49e76977ed5aaa757d5795
Coralie19 <coralie.daccord@gmail.com>
2019-08-17 11:04:52 +0200

    routing adapted to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,29 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { ProjectPage, ProjectDetail, OrgDetail, AuthPage } from './containers';
-import { NotFound } from './components';
+// import { NotFound } from './components';
 import ComponentsList from './styled-components/ComponentsList';
 import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
+  const DefaultContainer = () => (
+    <div>
+      <Navbar />
+      <Route exact path='/projects' component={ProjectPage} />
+      <Route exact path='/projects/city/:cityName' component={ProjectPage} />
+      <Route exact path='/projects/:projectId' component={ProjectDetail} />
+      <Route exact path='/orgs/:orgId' component={OrgDetail} />
+      <Route exact path='/styled-comps' component={ComponentsList} />
+      {/* <Route component={NotFound} /> */}
+    </div>
+  );
+
   return (
     <div className='App'>
-      <div>
-        <Navbar />
-      </div>
       <Switch>
         <Route exact path='/' component={AuthPage} />
-        <Route exact path='/projects' component={ProjectPage} />
-        <Route exact path='/projects/city/:cityName' component={ProjectPage} />
-        <Route exact path='/projects/:projectId' component={ProjectDetail} />
-        <Route exact path='/orgs/:orgId' component={OrgDetail} />
-        <Route exact path='/styled-comps' component={ComponentsList} />
-        <Route component={NotFound} />
+        <Route component={DefaultContainer} />
       </Switch>
     </div>
   );
