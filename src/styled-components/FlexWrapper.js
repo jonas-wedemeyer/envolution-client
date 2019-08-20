3d9e39b009ea8d32854c627e5e595a10ccb6f92c
jonas-wedemeyer <jo.wedemeyer@gmail.com>
2019-08-20 15:52:23 +0200

    feat/chore: updating styled comps

--- src/styled-components/FlexWrapper.js
+++ src/styled-components/FlexWrapper.js
@@ -2,6 +2,13 @@ import styled from 'styled-components';
 
 const FlexWrapper = styled.div`
   display: flex;
+  flex-direction: ${(props) => props.direction};
+  justify-content: ${(props) => props.justify};
+  align-items: ${(props) => props.align};
+  flex-grow: ${(props) => props.grow};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  z-index: ${(props) => props.zIndex};
   padding: 5px;
 `;
 
