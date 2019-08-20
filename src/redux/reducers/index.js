cb0a2bc452fa0bcbeab4c105d00791e88d912ef9
miquintero <schaeffer.quintero@gmail.com>
2019-08-20 19:38:09 +0200

    chore: imported and exported users reducer in index

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import projects from './projects/reducer';
 import organizations from './orgs/reducer';
+import users from './users/reducer';
 import authentication from './authentication/reducer';
 
 export default combineReducers({
   authentication,
   projects,
   organizations,
+  users,
 });
