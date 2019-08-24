49bb3c53230b30b82ec916548f5ba9d4d7beb24f
Jonas Wedemeyer <jonas@Jonass-MBP.lan>
2019-08-24 11:10:51 +0200

    chore: removed unused containers

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,3 @@
-import AddProject from './AddProject';
 import AuthPage from './AuthPage';
 import NewProject from './NewProject';
 import Onboarding from './Onboarding';
@@ -6,10 +5,8 @@ import OrgDetail from './OrgDetail';
 import ProjectPage from './ProjectPage';
 import ProjectDetail from './ProjectDetail';
 import UserProfile from './UserProfile';
-import UserProjects from './UserProjects';
 
 export {
-  AddProject,
   AuthPage,
   NewProject,
   ProjectPage,
@@ -17,5 +14,4 @@ export {
   Onboarding,
   OrgDetail,
   UserProfile,
-  UserProjects,
 };
